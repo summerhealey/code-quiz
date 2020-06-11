bab6319f56cb46e5ee46624de6dae75d21af57e7
Summer Healey <summerleigh.healey@gmail.com>
2020-06-11 10:34:23 -0400

    Created framework for quiz. HTML has been outlined with a header for the timer and 'View Highscore' button and in the body of the page there are four sections inside of the main tags for Code Content and include the Start Display, Questions and Answer Display, Highscore Input Area, and Highscore Display. CSS includes font stylings and the list of questions that the quiz will be built around has been included in the JavaScript variables.

---
+++ assets/script.js
@@ -0,0 +1,57 @@
+//Assignment Code
+
+//Variables
+
+const questions = [
+    {
+      question: `Where in our code do we keep the styling?`,
+      answers: [
+        `in a style.css file`,
+        `between two style tags`,
+        `inside our html with style attribute`,
+        `all of the above`,
+      ],
+      correctAnswer: `all of the above`,
+    },
+    {
+      question: `Why so JavaScript and Java have similar name?`,
+      answers: [
+        `JavaScript is a stripped-down version of Java`,
+        `JavaScript's syntax is loosely based on Java's`,
+        `They both originated on the island of Java`,
+        `None of the above`,
+      ],
+      correctAnswer: `JavaScript's syntax is loosely based on Java's`,
+    },
+    {
+      question: `When a user views a page containing a JavaScript program, which machine actually executes the script?`,
+      answers: [
+        `The User's machine running a Web browser`,
+        `The Web server`,
+        `A central machine deep within Netscape's corporate offices`,
+        `None of the above`,
+      ],
+      correctAnswer: `The User's machine running a Web browser`,
+    },
+    {
+      question: `______ JavaScript is also called client-side JavaScript.`,
+      answers: [`Microsoft`, `Navigator`, `LiveWire`, `Native`],
+      correctAnswer: `Navigator`,
+    },
+    {
+      question: `__________ JavaScript is also called server-side JavaScript.`,
+      answers: [`Microsoft`, `Navigator`, `LiveWire`, `Native`],
+      correctAnswer: `LiveWire`,
+    },
+    {
+      question: `What are variables used for in JavaScript Programs?`,
+      answers: [
+        `Storing numbers, dates, or other values`,
+        `Varying randomly`,
+        `Causing high-school algebra flashbacks`,
+        `None of the above`,
+      ],
+      correctAnswer: `Storing numbers, dates, or other values`,
+    },
+  ];
+  
