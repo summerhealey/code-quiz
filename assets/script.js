2cbd241630c954991e25391c3e1c86047e2da3eb
Summer Healey <summerleigh.healey@gmail.com>
2020-06-11 12:09:01 -0400

    Defined checkAnswer function to display of Incorrect or Correct on the page instead of as an alert.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 //Assignment Code
 
 //Variables
+let index = 0;
 const timer = document.querySelector("#time");
 let secondsLeft = 75;
 
@@ -99,4 +100,75 @@ startButton.addEventListener("click", function (event) {
     //Start Questions
     createQuestionDisplay();
   });
+
+//Q&A FUNCTION
+/*Function Description: Creates Question and Answer Area. The questions are displayed from the questions array, 
+then the answers that correlate to that question are displayed as a list of buttons,so the user can choose an answer.
+Once the answer is choosen the checkAnswer function determines if an answer is correct or incorrect.*/
+
+function createQuestionDisplay() {
+    const ul = document.getElementById("qa");
+    const currentQ = questions[index];
+    ul.textContent = currentQ.question;
+    console.log(currentQ);//Test
+    for (let i = 0; i < currentQ.answers.length; i++) {
+      var answerChoices = currentQ.answers[i];
+      const li = document.createElement("li");
+      var button = document.createElement("button");
+      button.setAttribute("value", answerChoices);
+      button.textContent = answerChoices;
+      button.className += "btn btn-primary";
+      ul.appendChild(li);
+      li.appendChild(button);
+      button.onclick = checkAnswer;
+    }
+  }
+
+//CHECK ANSWER FUNCTION
+/*Function Description: Determines if the answer the user provides matches the given correct answer 
+and returns "incorrect" or "correct". If incorrect,  in addition to display of "incorrect, 10 seconds are deducted 
+from the timer, if correct the answer is provided, the user is displayed "correct" and the quiz continues until the timer 
+displays 0 or all the questions are answered (correctly or incorrectly)*/
+
+function checkAnswer() {
+    if (this.value !== questions[index].correctAnswer) {
+      console.log(this.value);
+      checkAnswerDisplay.textContent = "Incorrect";
+      checkAnswerDisplay.style.textAlign ="left";
+      answerCheck.appendChild(checkAnswerDisplay);
+      secondsLeft = secondsLeft - 10; //Time deduction
+      console.log(secondsLeft); //Test
+      timerEl.textContent = seconds;
+      if (secondsLeft <= 0) {
+        secondsLeft = 0;
+        endQuiz();
+      }
+    } else {
+      checkAnswerDisplay.textContent = "Correct";
+      checkAnswerDisplay.style.textAlign ="left";
+      answerCheck.appendChild(checkAnswerDisplay);
+    }
+    index++;
+    if (index === questions.length) {
+      endQuiz();
+    }
+    createQuestionDisplay();
+  }
+  
+
+//END QUIZ FUNCTION
+/*Function Description: */ 
+
+function endQuiz() {
+    /* Hide quiz instructions and start button as well as the questions and answers 
+    - including the incorrect or correct display at the buttom of the page*/
+      quizHeader.style.display = "none";
+      startDisplay.style.display = "none";
+      qa.style.display = "none";
+      answerCheck.style.display = "none";
+      alert("Game Over");
+    }
+    
+  
+  
   
